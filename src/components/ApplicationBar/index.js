59998c8c555206a6bbc3f6ca7209b828a6e946e0
ye win <yewin.aung@zigway.co>
2020-12-09 17:51:35 +0630

    add style for header and footer

--- src/components/ApplicationBar/index.js
+++ src/components/ApplicationBar/index.js
@@ -6,36 +6,39 @@ import logo from '../../assets/img/logo-codigo-red.svg';
 class ApplicationBar extends Component {
     render() { 
         return (
-            <header className="app-nav">
-                <div className="logo">
-                    <img src={logo} className="App-logo" alt="logo" />
+            <header >
+                
+                <div className="app-nav">
+                    <div className="logo">
+                        <img src={logo} className="App-logo" alt="logo" />
+                    </div>
+                    <nav className="nav-list">
+                        <ul>
+                            <li>
+                                <Link to="/work">Work</Link>
+                            </li>
+                            <li>
+                                <Link to="/solutions">Solutions</Link>
+                            </li>
+                            <li>
+                                <Link to="/services">Services</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">about us</Link>
+                            </li>
+                            <li>
+                                <Link to="/blog">Blog</Link>
+                            </li>
+                            <li className="quote">
+                                <Link to="/blog">
+                                    <button>
+                                        Request a quote
+                                    </button>
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-                <nav className="nav-list">
-                    <ul>
-                        <li>
-                            <Link to="/work">Work</Link>
-                        </li>
-                        <li>
-                            <Link to="/solutions">Solutions</Link>
-                        </li>
-                        <li>
-                            <Link to="/services">Services</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">about us</Link>
-                        </li>
-                        <li>
-                            <Link to="/blog">Blog</Link>
-                        </li>
-                        <li>
-                            <Link to="/blog">
-                                <button>
-                                    Request a quote
-                                </button>
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
             </header>
         );
     }
--- src/components/SectionCategories/index.js
+++ src/components/SectionCategories/index.js
@@ -7,7 +7,7 @@ class SectionCategories extends Component {
         return (
             <div className="section-categories">
                 <CategoryItem imgUrl={'/static/media/CDG-2-1.e310a9c9.jpg'}/>
-                <CategoryItem imgUrl={'/static/media/FF-1.f99ab7ef.jpg'}/>
+                {/* <CategoryItem imgUrl={'/static/media/FF-1.f99ab7ef.jpg'}/> */}
                 <CategoryItem imgUrl={'/static/media/CDG-2-1.e310a9c9.jpg'}/>
                 <CategoryItem imgUrl={'/static/media/CDG-2-1.e310a9c9.jpg'}/>
                 <CategoryItem imgUrl={'/static/media/CDG-2-1.e310a9c9.jpg'}/>
